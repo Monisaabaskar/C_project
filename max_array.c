732547a0f196d2194680ba603a2bcd85bba40ab0
monishabaskar <bmonisaa9@gmail.com>
2023-09-23 01:06:50 +0200

    use of malloc

--- max_array.c
+++ max_array.c
@@ -1,14 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
 
 int max(int array[], int len);
 
-int main(void)
+int main(int argc , char *argv[])
 {
-    int ele[] = {13, 9, 16, 78, 100};
-    int len = 5;
+    if (argc > 2)
+    {
+        int len = strtol(argv[1], NULL, 10);
 
-    printf("maximum : %d\n" ,max(ele , len));
+        int *array = (int*)malloc(len * sizeof(int));
 
+        if(array == NULL)
+        {
+            return -1;
+        }
+        
+        else
+        {
+            for(int i = 0, j = 2; i < len; i++, j++)
+            {
+                array[i]= strtol(argv[j], NULL, 10);
+            }
+        }
+
+        printf("maximum : %d\n" ,max(array, len));
+
+    }
+    
     return 0;
 }
 
